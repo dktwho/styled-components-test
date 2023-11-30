ed36c4087b3e6e58d56f3c1982e53b52a833db19
Alexander Korobov <dktwho@mail.ru>
2023-11-30 11:10:22 +0300

    create StyledBtn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
+import styled from "styled-components";
+
 
 function App() {
     return (
         <>
-            start
+            <StyledBtn>Styled BTN</StyledBtn>
         </>
     )
 }
 
 export default App
+
+
+const StyledBtn = styled.button`
+  border: none;
+  background-color: greenyellow;
+  padding: 10px 20px;
+  border-radius: 5px;
+  color: snow;
+  font-size: 1.3rem;
+  font-weight: bold;
+`
