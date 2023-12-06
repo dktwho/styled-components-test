c9e45690d4ff454c4b4c22f57edd22f8046c27c2
Alexander Korobov <dktwho@mail.ru>
2023-12-06 14:11:31 +0300

    add Theme styled for change body color

--- src/styles/theme/Theme.styled.tsx
+++ src/styles/theme/Theme.styled.tsx
@@ -2,6 +2,7 @@ export const myTheme = {
     colors: {
         primary: 'gold',
         secondary: 'pink',
+        success: 'greenyellow',
         grey: {
             dark: "#2f2c2c",
             light: "#d2d2d2",
